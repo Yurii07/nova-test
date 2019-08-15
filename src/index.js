4e825b5c927ebc7c99aa0379691d497201977dc7
Yurii Adamenko <skyey07@gmail.com>
2019-08-15 23:43:02 +0300

    refresh design

--- src/index.js
+++ src/index.js
@@ -1,54 +1,32 @@
 import './js/common'
 // import './css/main.css'
-import './scss/main.scss'
 
 // import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
 // import 'bootstrap/dist/css/bootstrap.min.css'
-
 // import 'vue'
 // import Vue from 'vue'
 // Vue.use();
 // window.Vue = require('vue');
 
-
 import 'owl.carousel/dist/assets/owl.carousel.min.css';
-import 'owl.carousel';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
+
+import './scss/main.scss'
 
 $(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
+    $(".dropdown-trigger").dropdown(); //activate dropdown list
+    $('ul.tabs').tabs(); // dropdown nav
 
-        autoplay: true,
-        lazyLoad: true,
-        loop: true,
-        margin: 20,
-        /*
-       animateOut: 'fadeOut',
-       animateIn: 'fadeIn',
-       */
-        responsiveClass: true,
-        autoHeight: true,
-        dotsEach: true,dots: true,
-        autoplayTimeout: 5000,
-        smartSpeed: 800,
-        nav: true,
-        responsive: {
-        0: {
-            items: 1
-        },
-
-        600: {
-            items: 3
-        },
-
-        1024: {
-            items: 4
-        },
-
-        1366: {
-            items: 4
-        }
-    }
-
-
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true,
+        dragged: true
     });
+    autoplay()
+    function autoplay() {
+        $('.carousel').carousel('next');
+        setTimeout(autoplay, 4500);
+    }
 });
